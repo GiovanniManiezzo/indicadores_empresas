6770f67845709801239d2cfcd37b7aedca66fa8e
Giovanni Maniezzo <giovannimaniezzo@gmail.com>
2025-08-06 16:58:00 -0300

    Initial commit - spring initializer

---
+++ src/main/java/com/gm/indicadores_empresas/IndicadoresEmpresasApplication.java
@@ -0,0 +1,13 @@
+package com.gm.indicadores_empresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndicadoresEmpresasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndicadoresEmpresasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gm/indicadores_empresas/IndicadoresEmpresasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.indicadores_empresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndicadoresEmpresasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
